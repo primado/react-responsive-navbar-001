9b31db4cd76fd3a06ec01ad2a57dbdfe0cb9d58a
Nathaniel Quansah <n.bongo40@gmail.com>
2022-12-12 11:39:26 +0000

    converted class to function component

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,89 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, { Component, useState } from "react";
+import "./App.css";
 
-class Navbar extends Component {
+function Navbar() {
+  const [clicked, setClicked] = useState(false);
 
-  state = { clicked: false };
+  const handleClick = () => {
+    setClicked((prevState) => !prevState);
+  };
 
-  handleClick = () => {
-    this.setState({clicked: !this.state.clicked })
-  }
-  
-  render() {
-    return (
-      <div className="App">
-        <header id>
-          <nav>
-
-            <div >
-            <ul  id='navbar'className={ this.state.clicked ? "ul active": "ul"}>
-              <li><a href="#">Home</a></li>
-              <li><a href="#">Blog</a></li>
-              <li><a href="#">Contact</a></li>
-              <li><a href="#">About</a></li>
-
+  return (
+    <div className="App">
+      <header id>
+        <nav>
+          <div>
+            <ul id="navbar" className={clicked ? "ul active" : "ul"}>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
             </ul>
-            </div>
-            <button id='mobile' onClick= {  this.handleClick }>
-              <i id='bar' className={ this.state.clicked ? "fa-solid fa-xmark" : "fa-solid fa-bars" }></i>
-            </button>
-
-
-          </nav>
-        </header>
-      </div> 
-
-    )
-  }
+          </div>
+          <button id="mobile" onClick={handleClick}>
+            <i
+              id="bar"
+              className={clicked ? "fa-solid fa-xmark" : "fa-solid fa-bars"}
+            ></i>
+          </button>
+        </nav>
+      </header>
+    </div>
+  );
 }
 
-export default Navbar
+// class Navbar extends Component {
+//   state = { clicked: false };
+
+//   handleClick = () => {
+//     this.setState({ clicked: !this.state.clicked });
+//   };
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <header id>
+//           <nav>
+//             <div>
+//               <ul
+//                 id="navbar"
+//                 className={this.state.clicked ? "ul active" : "ul"}
+//               >
+//                 <li>
+//                   <a href="#">Home</a>
+//                 </li>
+//                 <li>
+//                   <a href="#">Blog</a>
+//                 </li>
+//                 <li>
+//                   <a href="#">Contact</a>
+//                 </li>
+//                 <li>
+//                   <a href="#">About</a>
+//                 </li>
+//               </ul>
+//             </div>
+//             <button id="mobile" onClick={this.handleClick}>
+//               <i
+//                 id="bar"
+//                 className={
+//                   this.state.clicked ? "fa-solid fa-xmark" : "fa-solid fa-bars"
+//                 }
+//               ></i>
+//             </button>
+//           </nav>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+
+export default Navbar;
